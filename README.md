c2aeca2d0e6c3e2dd402aace3016483f1a33a028
iamvee <naeini.v@gmail.com>
2021-02-09 23:04:36 +0330

    persian words added

--- README.md
+++ README.md
@@ -1,27 +1,61 @@
 # namegen
 تولید اسم های رندوم فینگیلیش
 
+## دو زبانه
 
 ```shell
-➜ for i in $( seq 20 ); do { ./main.py; sleep .3 } done
-grip_froot-e-salkhodeh
-zalzalak-e-taftideh
-khormaloo-ye-dana
-toranj-e-pasandideh
-beh-e-chasbideh
-azgil-e-varparideh
-taalebi-e-kar_amad
-zard_aloo-ye-paloodeh
-zoghal_akhte-ye-dars_khaandeh
-zalzalak-e-aloodeh
-gilas-e-varparideh
-porteghal-e-zir_afkan
-nargil-e-resideh
-narengi-e-rang_parideh
-khormaloo-ye-farsoodeh
-khiyar-e-farifteh
-kharboze-ye-afrookhteh
-shalil-e-bargozideh
-kharboze-ye-khak_barsar
-golabi-ye-dars_khaandeh
+➜ namegen -n 10
+شلیل پریشانتوت فرنگی بی ناموس
+toot farangi-ye bi_namoos
+غورهٔ خسته
+ghoore-ye khaste
+انار زره پوش
+anaar-e zereh push
+انگور سر سپرده
+angoor-e sar sepordeh
+زلزلک نادان
+zalzalak-e naadaan
+سیب ورپریده
+sib-e varparideh
+فندق عقدهای
+fandogh-e oghde_ee
+گریپ فروت قزمیت
+grip froot-e ghozmit
+نارگیل پرورده
+nargil-e parvardeh
+تن ماهی تافته
+ton maahi-ye tafteh
+```
+
+## فقط انگلیسی
+
+```shell
+➜ namegen -n 10 en
+shaftaaloo-ye boland parvaz
+baalang-e khoshhal
+porteghal-e foroo_heshteh
+piyaz-e oghde_ee
+fandogh-e afsordeh
+gheisi-e kar azmoodeh
+piyaz-e gomashteh
+limoo shirin-e zhoolideh
+senjed-e tafteh
+ananas-e khafan
+```
+
+## فقط فارسی
+
+```shell
+➜ namegen -n 10 fa
+
+کیوی شارلاتان
+لوبیا چیتی چابک
+نوشابهٔ مفلس
+تن ماهی اسکل
+انار آلوده
+انجیر پرورده
+توت خسته
+خیار رهیده
+کمبوزهٔ شراب زده
+گلابی فرسوده
 ```
--- main.py
+++ main.py
@@ -1,59 +1,201 @@
 #!/usr/bin/env python3
+import sys
+import time
+from secrets import choice
 
-""" Generates random name """
+def choice(objs):
+    indx = int(time.time() * 1_000_000) % len(objs)
+    return objs[indx]
 
-from secrets import choice as secrets_choice
-
-FOODS = [
-    'anaar-e', 'azgil-e', 'albaloo-ye', 'aloo-ye', 'ananas-e',
-    'anjir-e', 'angoor-e', 'avokaado-ye', 'badoom-e', 'baalang-e',
-    'baloot-e', 'beh-e', 'porteghal-e', 'peste-ye', 'toranj-e',
-    'tamr_hendi-ye', 'anbe-ye', 'tameshk-e', 'toot_farangi-ye',
-    'toot-e', 'kharboze-ye', 'khormaloo-ye', 'khorma-ye', 'khiyar-e',
-    'zoghal_akhte-ye', 'zalzalak-e', 'zard_aloo-ye', 'zereshk-e', 'zeytoon-e',
-    'senjed-e', 'sib-e', 'shaatoot-e', 'shaftaaloo-ye', 'shalil-e', 'taalebi-e',
-    'fandogh-e', 'gheisi-e', 'komboze-ye', 'kivi-ye', 'gerdoo-ye',
-    'grip_froot-e', 'golabi-ye', 'alooche-ye', 'gilas-e', 'limoo_torsh-e',
-    'limoo_shirin-e', 'nargil-e', 'movz-e', 'narengi-e', 'holoo-ye', 'hendoone-ye',
-    'karafs-e', 'shivid-e', 'torob-e', 'piyaz-e', 'sir-e', 'havij-e',
-    'nokhod-e', 'lobooya_sefid-e', 'loobiya_chiti-ye', 'ghoore-ye',
-    'kalam-e', 'kadoo_halavee-ye', 'delester-e', 'nooshabe-ye',
-    'panir-e', 'tah_chin-e', 'ton_maahi-ye', 'faloode-ye', 'shir_berenj-e'
+FOODS =[
+    {'en': 'anaar-e', 'fa': 'انار'},
+    {'en': 'azgil-e', 'fa': 'ازگیل'},
+    {'en': 'albaloo-ye', 'fa': 'آلبالوی'},
+    {'en': 'aloo-ye', 'fa': 'آلوی'},
+    {'en': 'ananas-e', 'fa': 'آناناس'},
+    {'en': 'anjir-e', 'fa': 'انجیر'},
+    {'en': 'angoor-e', 'fa': 'انگور'},
+    {'en': 'avokaado-ye', 'fa': 'آوکادوی'},
+    {'en': 'badoom-e', 'fa': 'بادوم'},
+    {'en': 'baalang-e', 'fa': 'بالنگ'},
+    {'en': 'baloot-e', 'fa': 'بلوط'},
+    {'en': 'beh-e', 'fa': 'به'},
+    {'en': 'porteghal-e', 'fa': 'پرتغال'},
+    {'en': 'peste-ye', 'fa': 'پستهٔ'},
+    {'en': 'toranj-e', 'fa': 'ترنج'},
+    {'en': 'tamr hendi-ye', 'fa': 'تمر هندی'},
+    {'en': 'anbe-ye', 'fa': 'انبهٔ'},
+    {'en': 'tameshk-e', 'fa': 'تمشک'},
+    {'en': 'toot farangi-ye', 'fa': 'توت فرنگی'},
+    {'en': 'toot-e', 'fa': 'توت'},
+    {'en': 'kharboze-ye', 'fa': 'خربزهٔ'},
+    {'en': 'khormaloo-ye', 'fa': 'خرمالوی'},
+    {'en': 'khorma-ye', 'fa': 'خرمای'},
+    {'en': 'khiyar-e', 'fa': 'خیار'},
+    {'en': 'zoghal akhte-ye', 'fa': 'ذغال اختهٔ'},
+    {'en': 'zalzalak-e', 'fa': 'زلزلک'},
+    {'en': 'zard aloo-ye', 'fa': 'زردآلوی'},
+    {'en': 'zereshk-e', 'fa': 'زرشک'},
+    {'en': 'zeytoon-e', 'fa': 'زیتون'},
+    {'en': 'senjed-e', 'fa': 'سنجد'},
+    {'en': 'sib-e', 'fa': 'سیب'},
+    {'en': 'shaatoot-e', 'fa': 'شاتوت'},
+    {'en': 'shaftaaloo-ye', 'fa': 'شفتالوی'},
+    {'en': 'shalil-e', 'fa': 'شلیل'},
+    {'en': 'taalebi-e', 'fa': 'طالبی'},
+    {'en': 'fandogh-e', 'fa': 'فندق'},
+    {'en': 'gheisi-e', 'fa': 'قیسی'},
+    {'en': 'komboze-ye', 'fa': 'کمبوزهٔ'},
+    {'en': 'kivi-ye', 'fa': 'کیوی'},
+    {'en': 'gerdoo-ye', 'fa': 'گردوی'},
+    {'en': 'grip froot-e', 'fa': 'گریپ فروت'},
+    {'en': 'golabi-ye', 'fa': 'گلابی'},
+    {'en': 'alooche-ye', 'fa': 'آلوچهٔ'},
+    {'en': 'gilas-e', 'fa': 'گیلاس'},
+    {'en': 'limoo torsh-e', 'fa': 'لیمو ترش'},
+    {'en': 'limoo shirin-e', 'fa': 'لیمو شیرین'},
+    {'en': 'nargil-e', 'fa': 'نارگیل'},
+    {'en': 'movz-e', 'fa': 'موز'},
+    {'en': 'narengi-e', 'fa': 'نارنگی'},
+    {'en': 'holoo-ye', 'fa': 'هلوی'},
+    {'en': 'hendoone-ye', 'fa': 'هندوانهٔ'},
+    {'en': 'karafs-e', 'fa': 'کرفس'},
+    {'en': 'shivid-e', 'fa': 'شوید'},
+    {'en': 'torob-e', 'fa': 'ترب'},
+    {'en': 'piyaz-e', 'fa': 'پیاز'},
+    {'en': 'sir-e', 'fa': 'سیر'},
+    {'en': 'havij-e', 'fa': 'هویج'},
+    {'en': 'nokhod-e', 'fa': 'نخود'},
+    {'en': 'loobiya sefid-e', 'fa': 'لوبی سفید'},
+    {'en': 'loobiya chiti-ye', 'fa': 'لوبیا چیتی'},
+    {'en': 'ghoore-ye', 'fa': 'غورهٔ'},
+    {'en': 'kalam-e', 'fa': 'کلم'},
+    {'en': 'kadoo halvaee-ye', 'fa': 'کدو حلوایی'},
+    {'en': 'delester-e', 'fa': 'دلستر'},
+    {'en': 'nooshabe-ye', 'fa': 'نوشابهٔ'},
+    {'en': 'panir-e', 'fa': 'پنیر'},
+    {'en': 'tah chin-e', 'fa': 'ته چین'},
+    {'en': 'ton maahi-ye', 'fa': 'تن ماهی'},
+    {'en': 'faloode-ye', 'fa': 'فالودهٔ'},
+    {'en': 'shir berenj-e', 'fa': 'شیر برنج'}
 ]
-""" List of foods """
 
 ADJS = [
-    'araasteh', 'ashofteh', 'asoodeh', 'aloodeh', 'afrookhteh', 'afsordeh',
-    'baaz_maandeh', 'bargozideh', 'paloodeh', 'pokhteh', 'parvardeh',
-    'parishan', 'parideh', 'pasandideh', 'pichideh', 'pishrafteh', 'bad_badan',
-    'tafteh', 'taftideh', 'chasbideh', 'khamideh', 'kharamideh', 'chegher',
-    'khalideh', 'khoshkideh', 'khaste', 'dars_khaandeh', 'darideh',
-    'dast_amooz', 'deldadeh', 'delpazir', 'domborideh', 'rahideh', 'ablah',
-    'rang_parideh', 'resideh', 'zereh_push', 'zabanzad', 'zir_afkan',
-    'zhoolideh', 'salkhodeh', 'sar_afkandeh', 'sar_sepordeh', 'sar_shekasteh',
-    'sar_gashteh', 'sookhteh', 'sharab_zadeh', 'shoorideh', 'shifteh',
-    'farrokhzad', 'farsoodeh', 'farifteh', 'foroo_heshteh', 'kar_azmoodeh',
-    'kar_amad', 'gosasteh', 'ghoshadeh', 'gozideh', 'gomashteh',
-    'legam_gosikhteh', 'miyaneh_ro', 'modabber', 'naadaan', 'sharlatan',
-    'naz_parvardeh', 'vabasteh', 'varafteh', 'varasteh', 'varparideh',
-    'varshekasteh', 'haraasaan', 'bi_namoos', 'khak_barsar', 'oghde_ee',
-    'kheng', 'dana', 'khoshhal', 'kheng', 'ghozmit', 'afsoreh_sima',
-    'boland_parvaz', 'delsard', 'kaftar_baaz', 'gorbe_baaz', 'chabok', 'khafan',
-    'daneshmand', 'data_scientist'
+    {'en': 'ashofteh', 'fa': 'آشفته'}, 
+    {'en': 'aloodeh', 'fa': 'آلوده'}, 
+    {'en': 'afsordeh', 'fa': 'افسرده'}, 
+    {'en': 'motor savar', 'fa': 'موتور سوار'},
+    {'en': 'bargozideh', 'fa': 'برگزیده'}, 
+    {'en': 'pokhteh', 'fa': 'پخته'}, 
+    {'en': 'parvardeh', 'fa': 'پرورده'}, 
+    {'en': 'shargاi', 'fa': 'شرقی'}, 
+    {'en': 'parishan', 'fa': 'پریشان'}, 
+    {'en': 'pichideh', 'fa': 'پیچیده'}, 
+    {'en': 'pishrafteh', 'fa': 'پیشرفته'}, 
+    {'en': 'gharb zadeh', 'fa': 'غرب زده'}, 
+    {'en': 'tafteh', 'fa': 'تافته'}, 
+    {'en': 'taftideh', 'fa': 'تفتیده'}, 
+    {'en': 'chasbideh', 'fa': 'چسبیده'}, 
+    {'en': 'khamideh', 'fa': 'خمیده'}, 
+    {'en': 'kharaaman', 'fa': 'خرامان'}, 
+    {'en': 'chegher', 'fa': 'چغر'}, 
+    {'en': 'khalideh', 'fa': 'خلیده'}, 
+    {'en': 'khaste', 'fa': 'خسته'}, 
+    {'en': 'dars khaandeh', 'fa': 'درس خوانده'}, 
+    {'en': 'deldadeh', 'fa': 'دلداده'}, 
+    {'en': 'delpazir', 'fa': 'دلپذیر'}, 
+    {'en': 'dom borideh', 'fa': 'دم بریده'}, 
+    {'en': 'rahideh', 'fa': 'رهیده'}, 
+    {'en': 'ablah', 'fa': 'ابله'}, 
+    {'en': 'rang parideh', 'fa': 'رنگ پریده'}, 
+    {'en': 'resideh', 'fa': 'رسیده'}, 
+    {'en': 'zereh push', 'fa': 'زره پوش'}, 
+    {'en': 'zabanzad', 'fa': 'زبانزد'}, 
+    {'en': 'zir afkan', 'fa': 'زیر افکن'}, 
+    {'en': 'zhoolideh', 'fa': 'ژولیده'}, 
+    {'en': 'salkhordeh', 'fa': 'سالخورده'}, 
+    {'en': 'sar afkandeh', 'fa': 'سرافکنده'}, 
+    {'en': 'sar sepordeh', 'fa': 'سر سپرده'}, 
+    {'en': 'sar shekasteh', 'fa': 'سر شکسته'}, 
+    {'en': 'sar gashteh', 'fa': 'سر گشته'}, 
+    {'en': 'sookhteh', 'fa': 'سوخته'}, 
+    {'en': 'sharab zadeh', 'fa': 'شراب زده'}, 
+    {'en': 'shoorideh', 'fa': 'شوریده'}, 
+    {'en': 'shifteh', 'fa': 'شیفته'}, 
+    {'en': 'farrokhzad', 'fa': 'فرخزاد'}, 
+    {'en': 'farsoodeh', 'fa': 'فرسوده'}, 
+    {'en': 'farifteh', 'fa': 'فریفته'}, 
+    {'en': 'foroo_heshteh', 'fa': 'فروهشته'}, 
+    {'en': 'kar azmoodeh', 'fa': 'کار آزموده'}, 
+    {'en': 'kar amad', 'fa': 'کارآمد'}, 
+    {'en': 'gosasteh', 'fa': 'گسسته'}, 
+    {'en': 'gozideh', 'fa': 'گزیده'}, 
+    {'en': 'gomashteh', 'fa': 'گماشته'}, 
+    {'en': 'legam gosikhteh', 'fa': 'لگام گسیخته'}, 
+    {'en': 'miyaneh ro', 'fa': 'میانه رو'}, 
+    {'en': 'modabber', 'fa': 'مدبر'}, 
+    {'en': 'naadaan', 'fa': 'نادان'}, 
+    {'en': 'sharlatan', 'fa': 'شارلاتان'}, 
+    {'en': 'naz parvardeh', 'fa': 'ناز پرورده'}, 
+    {'en': 'vabasteh', 'fa': 'وابسته'}, 
+    {'en': 'varafteh', 'fa': 'وارفته'}, 
+    {'en': 'varasteh', 'fa': 'وارسته'}, 
+    {'en': 'varparideh', 'fa': 'ورپریده'}, 
+    {'en': 'varshekasteh', 'fa': 'ورشکسته'}, 
+    {'en': 'haraasaan', 'fa': 'هراسان'}, 
+    {'en': 'bi_namoos', 'fa': 'بی ناموس'}, 
+    {'en': 'khak_barsar', 'fa': 'خاک برسر'}, 
+    {'en': 'oghde_ee', 'fa': 'عقده‌ای'}, 
+    {'en': 'kheng', 'fa': 'خنگ'}, 
+    {'en': 'dana', 'fa': 'دانا'}, 
+    {'en': 'khoshhal', 'fa': 'خوشحال'}, 
+    {'en': 'kheng', 'fa': 'خنگ'}, 
+    {'en': 'ghozmit', 'fa': 'قزمیت'}, 
+    {'en': 'afsordeh_sima', 'fa': 'افسرده سیما'}, 
+    {'en': 'boland parvaz', 'fa': 'بلند پرواز'}, 
+    {'en': 'delsard', 'fa': 'دلسرد'}, 
+    {'en': 'kaftar baaz', 'fa': 'کفترباز'}, 
+    {'en': 'chabok', 'fa': 'چابک'}, 
+    {'en': 'khafan', 'fa': 'خفن'}, 
+    {'en': 'daneshmand', 'fa': 'دانشمند'}, 
+    {'en': 'data scientist', 'fa': 'دیتا ساینتیست'}, 
+    {'en': 'mofles', 'fa': 'مفلس'}, 
+    {'en': 'dordi_kash', 'fa': 'دردی کش'}, 
+    {'en': 'sharaab_zadeh', 'fa': 'شراب زده'}, 
+    {'en': 'kharaab', 'fa': 'خراب'}, 
+    {'en': 'oskol', 'fa': 'اسکل'}, 
+    {'en': 'gij', 'fa': 'گیج'}
 ]
-""" List of adjectives """
 
-def gen() -> str:
+
+def gen(lang='fa') -> str:
     """ Generates a name and returns that as a string """
     adjs = ADJS * 10
     foods = FOODS * 10
-    name, adjective = secrets_choice(foods), secrets_choice(adjs)
+    
+    return f"{name[lang]} {adjective[lang]}"
 
-    return f"{name}-{adjective}"
-
-def run():
-    """ The main function """
-    print(gen())
-
+    
 if __name__ == '__main__':
-    run()
+
+
+    flags = sys.argv[1:]
+
+    if '-n' in flags:
+        ind = flags.index('-n')
+        count = int(flags.pop(ind+1))
+        flags.remove('-n')
+    else:
+        count = 1
+
+    for i in range(count):
+        name, adjective = choice(FOODS), choice(ADJS)
+        flags = ['fa', 'en'] if not flags else flags
+        if 'fa' in flags:
+            lang='fa'
+            print(f"{name[lang]} {adjective[lang]}")
+ 
+        if 'en' in flags:
+            lang='en'
+            print(f"{name[lang]} {adjective[lang]}")
+        
