3163d53b6b2358f899a864ae19786c9e6a82c6d7
parsa shahmaleki <parsampsh@gmail.com>
2021-02-11 11:38:30 +0330

    add a test script

---
+++ test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+""" Running the tests for karafs """
+
+import os
+import subprocess
+import platform
+import importlib.util
+
+KARAFS_BIN = os.path.abspath('bin/karafs')
+
+def run_command(cmd='') -> str:
+    """ Runs a command on karafs and returns the output as string """
+    py = 'python3'
+    if platform.system().startswith('Winodws'):
+        py = 'python'
+    return subprocess.check_output(py + ' ' + repr(KARAFS_BIN) + ' ' + cmd, shell=True).decode().strip()
+
+def test():
+    """ Test the program """
+    # run the command
+    output = run_command()
+    assert len(output.splitlines()) == 2
+
+    output = run_command('-n 10')
+    assert len(output.splitlines()) == 20
+
+    output = run_command('en')
+    assert len(output.splitlines()) == 1
+
+    output = run_command('fa')
+    assert len(output.splitlines()) == 1
+
+    output = run_command('fa -n 5')
+    assert len(output.splitlines()) == 5
+
+    output = run_command('en -n 5')
+    assert len(output.splitlines()) == 5
+
+    output = run_command('other')
+    assert len(output.splitlines()) == 0
+
+if __name__ == '__main__':
+    test()
+    print('Tests passed successfully.')
